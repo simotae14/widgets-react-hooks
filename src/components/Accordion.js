ce575d91c50e8e9a7f66ed22604ce89d542e5db1
Simone <startae14@gmail.com>
2021-04-30 16:02:34 +0200

    03: add the basic html for the Accordion component passing also all the data

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
+  const renderItems = items.map(item => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          { item.title }
+        </div>
+        <div className="content active">
+          <p>
+            { item.content }
+          </p>
+        </div>
+      </React.Fragment>
+    )
+  })
   return (
-    <h1>
-      { items.length }
-    </h1>
+    <div className="ui styled accordion">
+      { renderItems }
+    </div>
   );
 };
 
