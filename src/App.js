7d3d19061a421297d17f4cbbf450c01654218228
Simone <startae14@gmail.com>
2021-04-30 15:29:16 +0200

    02: create the items to show in the accordion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  }
+]
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   return (
-    <h1>
-      Widgets App
-    </h1>
+    <div>
+      <Accordion items={items} />
+    </div>
   );
 };
