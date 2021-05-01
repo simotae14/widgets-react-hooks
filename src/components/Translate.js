08894cb52fb0729e3db97d929149d36871488dca
Simone <startae14@gmail.com>
2021-05-02 00:12:30 +0200

    33: create the Convert Component that receives the selected language and the term to convert

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
   {
@@ -13,6 +14,14 @@ const options = [
   {
     label: 'Hindi',
     value: 'hi'
+  },
+  {
+    label: 'Dutch',
+    value: 'nl'
+  },
+  {
+    label: 'Italian',
+    value: 'it'
   }
 ];
 
@@ -32,6 +41,12 @@ const Translate = () => {
         options={options}
         selected={language}
         onSelectedChange={setLanguage}
+      />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert
+        text={text}
+        language={language}
       />
     </div>
   );
