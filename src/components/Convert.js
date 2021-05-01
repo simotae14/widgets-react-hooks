44b13b10f5fa9286c9777792752fab6728ef21aa
Simone <startae14@gmail.com>
2021-05-02 00:43:58 +0200

    34: add an axios post api to the google translate API passing some parameters and an env var for the API_KEY

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Convert = ({ language, text }) => {
   useEffect(() => {
-    console.log('New language or text');
+    axios.post(
+      'https://translation.googleapis.com/language/translate/v2',
+      {},
+      {
+        params: {
+          q: text,
+          target: language.value,
+          key: process.env.REACT_APP_GOOGLE_API_KEY
+        }
+      }
+    );
   }, [language, text]);
   return (
     <div>
