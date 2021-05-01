4d8ab8493a120cd00e1ccfdfda8542bea8423c0b
Simone <startae14@gmail.com>
2021-05-02 00:51:31 +0200

    35: create an async function inside the useEffect to call the api and memorize the response in a state variable

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,22 +2,28 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
   useEffect(() => {
-    axios.post(
-      'https://translation.googleapis.com/language/translate/v2',
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: process.env.REACT_APP_GOOGLE_API_KEY
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: process.env.REACT_APP_GOOGLE_API_KEY
+          }
         }
-      }
-    );
+      );
+
+      setTranslated(data.data.translations[0].translatedText);
+    }
+    doTranslation();
   }, [language, text]);
   return (
     <div>
-      Convert
+      <h1 className="ui header">{translated}</h1>
     </div>
   );
 };
