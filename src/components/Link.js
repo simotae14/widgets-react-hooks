03720ae5f812be727ea04870fefd53a2e5ed96c2
Simone <startae14@gmail.com>
2021-05-06 00:33:49 +0200

    43: create a custom event on click on a Link element and dispatch it, and add a useEffect to stay listening to the new callback

--- src/components/Link.js
+++ src/components/Link.js
@@ -5,6 +5,11 @@ const Link = ({ href, className, children }) => {
     event.preventDefault();
     // change the url in the browser bar
     window.history.pushState({}, '', href);
+
+    // create a custom popstate event
+    const navEvent = new PopStateEvent('popstate');
+    // dispatch del nuovo event
+    window.dispatchEvent(navEvent);
   }
   return (
     <a onClick={onClick} className={className} href={href}>
