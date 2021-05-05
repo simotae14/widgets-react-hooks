4f8345f7e0749bb2a9d7db7bab2fd29062c21d92
Simone <startae14@gmail.com>
2021-05-06 01:03:43 +0200

    44: add a state to refresh the content in the Route based on the changing of the window.location.pathname

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,9 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Route = ({path, children}) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
   useEffect(() => {
     const onLocationChange = () => {
-      console.log("Location change");
+      setCurrentPath(window.location.pathname);
     }
     window.addEventListener('popstate', onLocationChange);
 
@@ -12,7 +13,7 @@ const Route = ({path, children}) => {
       window.removeEventListener('popstate', onLocationChange);
     };
   }, []);
-  return window.location.pathname === path
+  return currentPath === path
     ? children
     : null;
 };
